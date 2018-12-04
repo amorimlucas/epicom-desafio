e17599411f23faacb0938026ee40679d446ec2e2
Lucas Amorim <amorimluc@gmail.com>
2018-12-04 14:06:59 -0200

    Adicionar testes para requisito 1

--- spec/requests/skus_spec.rb
+++ spec/requests/skus_spec.rb
@@ -8,11 +8,18 @@ RSpec.describe "Skus", type: :request do
 	    end
    	end
 
-
     context "testes para requisito 1 do Desafio Epicom" do
-    	it "PUT /api/v1/sku/:id" do
-
-    		#json = 
+    	let(:params) do {tipo: "sku_associado", dataEnvio: "2015-07-14T13:56:36", parametros: {idProduto: 100, idSku: 200}} end
+		let(:headers) do { CONTENT_TYPE: "application/json" } end
+		let(:sku1) do  
+			Sku.create id: 200, idProduto: 50 
+		end
+    	it "POST notificação para /api/v1" do
+    		post api_v1_path, params, headers
+    		
+			expect(response).to have_http_status(200)
+			expect(test_sku.reload.parametros.idProduto).to eq (200)
     	end
+
     end
 end
