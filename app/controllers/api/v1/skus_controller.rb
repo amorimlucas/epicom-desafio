d92cec97d5207a7f9130145d010115c1c6ce5ddc
Lucas Amorim <amorimluc@gmail.com>
2018-12-04 21:14:36 -0200

    Requisito 3: Implementar endpoint especial

--- app/controllers/api/v1/skus_controller.rb
+++ app/controllers/api/v1/skus_controller.rb
@@ -1,6 +1,12 @@
+require 'net/http'
+require 'net/https'
+require 'uri'
+require 'json'
+
 module Api
 	module V1
 		class SkusController < ApplicationController
+
 			def index 
 				skus = Sku.all
 				render json: {status: "SUCCESS", message: "Todos SKUs", data: skus}, status: :ok
@@ -39,6 +45,40 @@ module Api
 		        sku.destroy
 		        render json: {status: 'SUCCESS', message:'SKU deletado.', data: sku},status: :ok
 			end
+
+			def return_filtered_skus
+				filtered_skus = []
+				Sku.limit(20).each do |sku|
+					disponivel_endpoint = "marketplace/produtos/#{sku.idProduto}/skus/#{sku.id}/disponibilidade"
+					url = "https://sandboxmhubapi.epicom.com.br/v1/#{disponivel_endpoint}"
+					uri = URI(url)
+
+					disponivel = nil
+					preco = nil
+
+					Net::HTTP.start(uri.host, uri.port,
+					  :use_ssl => uri.scheme == 'https', 
+					  :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+
+					  request = Net::HTTP::Get.new uri.request_uri
+					  request.basic_auth '897F8D21A9F5A', 'Ip15q6u7X15EP22GS36XoNLrX2Jz0vqq'
+
+					  response = http.request request # Net::HTTPResponse object
+
+					  parsed = JSON.parse(response.body)
+					  disponivel = parsed["disponivel"]
+					  preco = parsed["preco"]
+
+					  filtered_skus.push(sku) if disponivel && (preco.to_f < 40.0 && preco.to_f > 10.0)
+					  
+					end
+
+				end
+				render json: {status: 'SUCCESS', message:'SKU TEST', data: filtered_skus},status: :ok
+				#data = JSON.parse("[#{filtered_skus.join(', ')}]")
+				#render json: {status: 'SUCCESS', message:'SKUs disponíveis e entre R$10.0 e R$40.0.', data: data}, status: :ok
+			end
+
 		end
 	end
 end
